679e5498c3f8eefec1843424e3bdef4ee2a1f70e
long1703@outlook.com <long1703@outlook.com>
2022-12-29 22:37:31 +0700

    Xây dựng UI phần Header #2

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,4 +1,7 @@
+import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import classNames from 'classnames/bind';
+import images from '~/assets/images';
 import styles from './Header.module.scss'
 
 
@@ -10,7 +13,27 @@ const cx = classNames.bind(styles);
 function Header() {
     return <header className={cx('wrapper')}>
         <div className={cx('inner')}>
+            <div className={cx('logo')}>
+                <img src={images.logo} alt='tt' />
+            </div>
+            <div className={cx('search')}>
+                <input placeholder="Search accounts and videos " spellCheck={false} />
+                <button>
+                    {/* clear */}
+                    <FontAwesomeIcon icon={faCircleXmark} />
+                </button>
+                <button>
+                    {/* loading */}
+                    <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+                </button>
+                <button className={cx('search-btn')}>
+                    {/* search */}
+<FontAwesomeIcon icon={faMagnifyingGlass} />
+                </button>
+            </div>
+            <div className={cx['actions']}>
 
+            </div>
         </div>
     </header>
 }
